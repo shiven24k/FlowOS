c62ee737f68d97ace58363fbafc6e4e8fd6bdf70
shiven24k <kashyapshiven2002@gmail.com>
2025-11-29 21:02:06 +0530

    feat:auth with connected database

--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,31 @@
 import express from "express";
 import cors from "cors";
 import { Router } from "express";
+import db from "./src/config/db"
+import auth from "./src/routes/auth-routes";
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middlewares
-app.use(cors({
-  origin:'http://localhost:8000/', // we will change to specific after hosting
-  credentials:true
-}
-));
+app.use(cors());
 app.use(express.json());
-app.use(Router);
+app.use("/api/auth",auth)
+//Routes
 
 
 app.get("/", (req, res) => {
   res.send("hey backend is working");
 });
 
-
+app.get("/get-db", async(req,res)=> {
+  try {
+    const result = await db.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  }catch(error){
+    console.log(error);
+  }
+})
 
 
 
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,19 @@
-import pkg from "pg";
-const {Pool} = pkg;
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const pool = new Pool({
+const connection = new Client({
+  // user:process.env.DATABASE_USERNAME,
+  // host:process.env.DATABASE_HOST,
+  // database:process.env.DATABASE_DATABASE,
+  // password:process.env.DATABASE_PASSWORD,
   connectionString: process.env.DATABASE_URL,
+  // port: 5432,
 });
+//create
+//read
+//update
+//delete
+connection.connect().then(()=>console.log("Connected schema"));
+
+export default connection
