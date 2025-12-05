ffd687ad560268f267b96f16619aa244a291963a
Sunehra Tabassum <sunehrat815@gmail.com>
2025-12-05 14:12:28 +0500

    shore : Fix Form container responsive styles

--- frontend/src/components/ui/form.tsx
+++ frontend/src/components/ui/form.tsx
@@ -1,10 +1,10 @@
 import { cva } from "class-variance-authority";
-import {cn} from '../../lib/utils'
+import { cn } from "../../lib/utils";
 
 const formContainerStyles = cva(
-  "flex flex-col gap-3 bg-white p-8 w-[450px] rounded-2xl"
+  "flex flex-col gap-3 bg-white w-[80vw] p-5 md:w-[450px] md:p-8  rounded-2xl"
 );
 
-export function FormContainer({ children, className }:any) {
+export function FormContainer({ children, className }: any) {
   return <div className={cn(formContainerStyles(), className)}>{children}</div>;
 }
